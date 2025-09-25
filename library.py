7cbef564760de6595b70b591afb1b30febdc38d9
Hitesh-Sharma1021 <hitu12345sharma@gmail.com>
2025-09-26 01:15:43 +0530

    Created table creator and input chart module

--- library.py
+++ library.py
@@ -1,5 +1,7 @@
 import mysql.connector as sqltor
-mycon=sqltor.connect(host="localhost",user="root",password="1021", database="project")
+# db=input("Enter name of the database : ")
+db="project"
+mycon=sqltor.connect(host="localhost",user="root",password="1021", database=db)
 
 def bdr():
     print()
@@ -11,7 +13,27 @@ def isconnected():
         return True
     else:
         return False
-
+def inputchart():
+    print("Field types Chart:\n1. For 'integer' enter 1\n2. For 'text' enter 2\n3. For 'date' enter 3\n4. For 'decimal values' enter 4")
+    opt=int(input("Enter the option : "))
+    if opt==1:
+        ln=input("Enter the length of the field : ")
+        fldtyp=f"int({ln})"
+        return fldtyp
+    elif opt==2:
+        ln=input("Enter the length of the field : ")
+        fldtyp=f"varchar({ln})"
+        return fldtyp
+    elif opt==3:
+        fldtyp=f"date"
+        return fldtyp
+    elif opt==4:
+        ln=input("Enter the length of the field : ")
+        dp=input("Enter the no of decimal points : ")
+        fldtyp=f"float({ln},{dp})"
+        return fldtyp
+    else:
+        print("Invalid Input !!")
 
 def executer(command,inp):
     list1=[]
@@ -25,7 +47,37 @@ def executer(command,inp):
     return list1 
 
 def tablecreator():
-    pass
+    print("The tables in our database are as follows : ")
+    executer("Show tables",True)
+    tbl=input("Enter the name of the table you want to create: ")
+    bdr()
+    ans="y"
+    pricount=0
+
+    data=f"create table {tbl} ("
+    while ans[0].upper()=="Y":
+        fldnm=input("Enter the field name : ")
+        fldtyp=inputchart()
+        if pricount==0:
+            prikey=input("Do you want to make this a primary key? : ")
+            if prikey[0].upper()=="Y":
+                iskey="Primary key"
+                pricount+=1
+        unikey=input("Do you want to make it unique key? : ")
+        if unikey[0].upper()=="Y":
+            iskey="Unique"      
+        iskey=" "
+        info=f"{fldnm}  {fldtyp}    {iskey}"
+        data+=info
+        ans=input("Do you want to continue? : ")
+        if ans[0].upper()=="Y":
+            data+=","
+        elif ans[0].upper()=="N":
+            data+=")"
+    executer(data,False)
+    bdr()
+    executer(f"describe {tbl}",True)
+            
 
 def searchbot():
     if isconnected():
@@ -45,4 +97,5 @@ def searchbot():
     else:
         print("The Database is not connected !!")
 
-searchbot()
+# searchbot()
+tablecreator()
